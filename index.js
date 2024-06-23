bdcdfcab499ccdb70f347dde6f91539f2fec4c33
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-06-23 10:48:59 +0500

    soome changes

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 let randomNum = Math.floor(Math.random() * 100);
+console.log("\n\t Number Guessing Game \t\n");
 let obj;
 do {
     obj = await inquirer.prompt([
         {
             name: "guessedNumber",
             type: "number",
-            message: "Enter a number",
+            message: "Guess number between 1 - 100 : ",
         },
     ]);
     if (randomNum > obj.guessedNumber) {
-        console.log("Number is Greater than your entered number");
+        console.log("Number is Greater than " + obj.guessedNumber);
     }
     else if (randomNum < obj.guessedNumber) {
-        console.log("Number is Lesser than your entered number");
+        console.log("Number is Lesser than " + obj.guessedNumber);
     }
     else {
         console.log("Yahoo! You guessed the right number...");
